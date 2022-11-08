d4d0399f97cd12c83bc21c136b83cdc48f6af71d
Jonathan Neugebauer <jonathan.neugebauer@wi.uni-muenster.de>
2022-11-08 10:55:36 +0100

    Refactor code

--- dacite-intellij/src/main/java/dacite/intellij/actions/DaciteAnalyzeAction.java
+++ dacite-intellij/src/main/java/dacite/intellij/actions/DaciteAnalyzeAction.java
@@ -1,46 +1,42 @@
 package dacite.intellij.actions;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.RegisterToolWindowTask;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
-import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
-import dacite.intellij.DaciteAnalysisLauncher;
-import dacite.intellij.visualisation.DaciteToolWindowFactory;
+
 import org.jetbrains.annotations.NotNull;
+
+import dacite.intellij.DaciteAnalysisLauncher;
 
 public class DaciteAnalyzeAction extends AnAction {
 
-    @Override
-    public void update(AnActionEvent e) {
-        // Using the event, evaluate the context, and enable or disable the action.
-    }
+  @Override
+  public void update(AnActionEvent e) {
+    // Using the event, evaluate the context, and enable or disable the action.
+  }
 
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        System.out.println("action performed");
-        Project project =e.getProject();
-        PsiFile file = e.getData(PlatformCoreDataKeys.PSI_FILE);
-        String filename = "";
-        String packagename = "";
-        if(file instanceof PsiJavaFile){
-            PsiJavaFile jfile = (PsiJavaFile) file;
-            packagename =jfile.getPackageName();
-            filename = packagename +"."+jfile.getName();
-            if(packagename.contains(".")){
-                packagename = packagename.replace(".","/");
-            }
-            filename = filename.substring(0,filename.lastIndexOf("."));
-        }
-        DaciteAnalysisLauncher.launch(project, packagename,filename);
-
-        // Using the event, implement an action. For example, create and show a dialog.
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    System.out.println("action performed");
+    Project project = e.getProject();
+    PsiFile file = e.getData(PlatformCoreDataKeys.PSI_FILE);
+    String filename = "";
+    String packagename = "";
+    if (file instanceof PsiJavaFile) {
+      PsiJavaFile jfile = (PsiJavaFile) file;
+      packagename = jfile.getPackageName();
+      filename = packagename + "." + jfile.getName();
+      if (packagename.contains(".")) {
+        packagename = packagename.replace(".", "/");
+      }
+      filename = filename.substring(0, filename.lastIndexOf("."));
     }
+    DaciteAnalysisLauncher.launch(project, packagename, filename);
+
+    // Using the event, implement an action. For example, create and show a dialog.
+  }
 
 }
