11c5feb7d2a8096d239c431451e6ad647ef446f0
Laur0r <laura-troost@t-online.de>
2022-11-09 14:51:25 +0100

    stop unnecessary calls to CodeLens

--- dacite-intellij/src/main/java/dacite/intellij/DaciteRunLineMarkerContributor.java
+++ dacite-intellij/src/main/java/dacite/intellij/DaciteRunLineMarkerContributor.java
@@ -37,37 +37,53 @@ import static org.wso2.lsp4intellij.requests.Timeout.getTimeout;
 import static org.wso2.lsp4intellij.requests.Timeouts.REFERENCES;
 
 public class DaciteRunLineMarkerContributor extends RunLineMarkerContributor {
+
+    private List<? extends CodeLens> codeLens;
+    private String textDocument;
+
     @Override
     public @Nullable Info getInfo(@NotNull PsiElement element) {
         int offset = element.getTextOffset();
         PsiDocumentManager m = PsiDocumentManager.getInstance(element.getProject());
         Document doc = m.getDocument(element.getContainingFile());
         int line = doc.getLineNumber(offset);
-        LogicalPosition elemPos = new LogicalPosition(line+1, offset-doc.getLineStartOffset(line));
+        LogicalPosition elemPos = new LogicalPosition(line + 1, offset - doc.getLineStartOffset(line));
         Set<LanguageServerWrapper> wrapper = IntellijLanguageClient.getAllServerWrappersFor(FileUtils.projectToUri(element.getProject()));
         RequestManager requestManager = null;
-        if(wrapper.size() == 1){
+        if (wrapper.size() == 1) {
             requestManager = wrapper.iterator().next().getRequestManager();
         }
-        CodeLensParams param = new CodeLensParams(new TextDocumentIdentifier(element.getContainingFile().getVirtualFile().getUrl()));
-        CompletableFuture<List<? extends CodeLens>> request = requestManager.codeLens(param);
-        if (request != null) {
-            try {
-                List<? extends CodeLens> list = request.get(getTimeout(REFERENCES), TimeUnit.MILLISECONDS);
-                for(CodeLens lens : list){
-                    LogicalPosition position = new LogicalPosition(lens.getRange().getStart().getLine()+1, lens.getRange().getStart().getCharacter());
-                    //int lensOffset = editor.logicalPositionToOffset(position);
-                    if(position.equals(elemPos)){
-                        return new Info(null, null,
-                                ActionManager.getInstance().getAction(lens.getCommand().getCommand()));
+        if (textDocument == null || !textDocument.equals(element.getContainingFile().getVirtualFile().getUrl())) {
+            CodeLensParams param = new CodeLensParams(new TextDocumentIdentifier(element.getContainingFile().getVirtualFile().getUrl()));
+            CompletableFuture<List<? extends CodeLens>> request = requestManager.codeLens(param);
+            if (request != null) {
+                try {
+                    List<? extends CodeLens> list = request.get(getTimeout(REFERENCES), TimeUnit.MILLISECONDS);
+                    textDocument = element.getContainingFile().getVirtualFile().getUrl();
+                    codeLens = list;
+                    for (CodeLens lens : list) {
+                        LogicalPosition position = new LogicalPosition(lens.getRange().getStart().getLine() + 1, lens.getRange().getStart().getCharacter());
+                        //int lensOffset = editor.logicalPositionToOffset(position);
+                        if (position.equals(elemPos)) {
+                            return new Info(null, null,
+                                    ActionManager.getInstance().getAction(lens.getCommand().getCommand()));
+                        }
                     }
+
+                } catch (TimeoutException | InterruptedException | JsonRpcException | ExecutionException e) {
+                    e.printStackTrace();
                 }
-
-            } catch (TimeoutException | InterruptedException | JsonRpcException | ExecutionException e) {
-                e.printStackTrace();
             }
+            return null;
+        } else {
+            for (CodeLens lens : codeLens) {
+                LogicalPosition position = new LogicalPosition(lens.getRange().getStart().getLine() + 1, lens.getRange().getStart().getCharacter());
+                if (position.equals(elemPos)) {
+                    return new Info(null, null,
+                            ActionManager.getInstance().getAction(lens.getCommand().getCommand()));
+                }
+            }
+            return null;
         }
-        return null;
     }
-
 }
